0e1230a21fc2e89b8d722fc06d39a1f0ab1f3324
Amar <amarsonone199@gmail.com>
2022-04-18 11:39:03 +0530

    adding a new amr project

---
+++ logical/src/logicalex/ReverseArray.java
@@ -0,0 +1,13 @@
+package logicalex;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+int[]a= {22,55,66,55,59};
+for (int i=a.length-1;i>=0;i--) {
+	System.out.println(a[i]);
+}
+
+	}
+
+}
---
+++ logical/src/logicalex/ReverseNo.java
@@ -0,0 +1,12 @@
+package logicalex;
+
+public class ReverseNo {
+
+	public static void main(String[] args) {
+int a=556582511;
+for (int i = a; i >= 0; i--) {
+	System.out.println(a);
+}
+	}
+
+}
